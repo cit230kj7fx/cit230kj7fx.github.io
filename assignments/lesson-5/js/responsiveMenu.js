d37e14858d6314dd03b5c48903ee6e0d41a5a692
Mike Odom <odommi@byui.edu>
2018-02-22 15:38:14 -0700

    Update to lesson 5

---
+++ assignments/lesson-5/js/responsiveMenu.js
@@ -0,0 +1,9 @@
+/* Toggle between show and hide */
+function myFunction() {
+    var x = document.getElementById("primaryNavigation");
+    if (x.className === "show") {
+        x.className = "hide";
+    } else {
+        x.className = "show";
+    }
+} // end of function
