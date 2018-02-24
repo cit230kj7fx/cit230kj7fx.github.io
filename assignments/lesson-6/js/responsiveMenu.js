3f122424557e6ec4d4247bbaf38eb488c6abc9cb
Mike Odom <odommi@byui.edu>
2018-02-23 20:27:11 -0700

    Updates for hamburger menu

--- assignments/lesson-6/js/responsiveMenu.js
+++ assignments/lesson-6/js/responsiveMenu.js
@@ -1,6 +1,6 @@
 /* Toggle between show and hide */
-function myFunction() {
-    var x = document.getElementById("primaryNavigation");
+function toggleNavMenu() {
+    var x = document.getElementById("nav-container");
     if (x.className === "show") {
         x.className = "hide";
     } else {
