8882f6cffc9323c3698968ce28f75aab6ad50826
Mike Odom <odommi@byui.edu>
2018-03-14 21:32:11 -0600

    Adding Current Data

---
+++ weather/js/wufranklin.js
@@ -0,0 +1,32 @@
+//Retrieve Weather Data for Franklin
+var weatherObject = new XMLHttpRequest();
+
+weatherObject.open('GET', '//api.wunderground.com/api/ef29207c30bfe9cd/conditions/q/MN/Franklin.json', true);
+
+weatherObject.send();
+
+weatherObject.onload = function() {
+    var weatherInfo = JSON.parse(weatherObject.responseText);
+        
+    console.log(weatherInfo);
+    document.getElementById('place').innerHTML = weatherInfo.current_observation.display_location.full;
+
+    document.getElementById('currentTemp').innerHTML = weatherInfo.current_observation.temp_f;
+    
+    document.getElementById('precip').innerHTML = weatherInfo.current_observation.precip_today_in;
+    
+    document.getElementById('windspeed').innerHTML = weatherInfo.current_observation.wind_mph;
+    
+    //Allow for function to work in https mode
+    var icon_path = weatherInfo.current_observation.icon_url;
+    var urlString = document.location.href;
+    var found = urlString.indexOf('https');
+    
+    if (found>=0) {
+        icon_path = icon_path.replace("http","https");
+    }
+    
+    document.getElementById('weather_icon').src =     
+   icon_path;
+
+} //end of onload function
