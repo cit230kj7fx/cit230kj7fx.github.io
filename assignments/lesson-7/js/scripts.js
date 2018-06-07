eb2b73d153538f3b0b7958c96de78c113cc75889
Mike Odom <odommi@byui.edu>
2018-06-07 10:34:06 -0600

    Wind Chill Updates

--- assignments/lesson-7/js/scripts.js
+++ assignments/lesson-7/js/scripts.js
@@ -49,7 +49,7 @@ var low = 66;
 var windSpeed = 5;
 
 
-var avereageTemp = ((high - low) / 2);
+var avereageTemp = ((high + low) / 2);
 var windChill = 35.74 + (0.6215 * avereageTemp) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * avereageTemp) * Math.pow(windSpeed, 0.16);
 
 windChill = Math.round(windChill) + "&deg;F";
