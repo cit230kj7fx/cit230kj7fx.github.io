aa97c1993a7957147e3c4eae5856400b62af29b3
Mike Odom <odommi@byui.edu>
2018-06-07 10:27:29 -0600

    Date Fixes

--- assignments/lesson-7/js/oldresponsiveMenu.js
+++
@@ -1,9 +0,0 @@
-/* Toggle between show and hide */
-function toggleNavMenu() {
-    var x = document.getElementById("nav-container");
-    if (x.className === "show") {
-        x.className = "hide";
-    } else {
-        x.className = "show";
-    }
-} // end of function
--- assignments/lesson-7/js/scripts.js
+++ assignments/lesson-7/js/scripts.js
@@ -36,8 +36,8 @@ var theDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
 var theMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 var d = new Date();
+var dayName = theDays[d.getDay()];
 var day = d.getDate();
-var dayName = theDays[day];
 var monthName = theMonths[d.getMonth()];
 var year = d.getFullYear();
 
