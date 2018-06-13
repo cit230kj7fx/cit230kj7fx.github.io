b3595805202ac6639870ddd0cb1b25964c1a9211
Mike Odom <odommi@byui.edu>
2018-06-13 15:33:03 -0600

    Updates

--- assignments/lesson-8/js/scripts.js
+++ assignments/lesson-8/js/scripts.js
@@ -1,3 +1,19 @@
+// --------------- GENERATE DATE SCRIPT --------------------
+var theDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+var theMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var d = new Date();
+var dayName = theDays[d.getDay()];
+var day = d.getDate();
+var monthName = theMonths[d.getMonth()];
+var year = d.getFullYear();
+
+document.getElementById("currentdate").innerHTML = dayName + ", " + day + " " + monthName + " " + year;
+
+var test = dayName + ", " + day + " " + monthName + " " + year;
+
+
 // ---------- WAYFINDING TO HIGHLIGHT ACTIVE MENU ITEM -----------------
 //remember to add an ID of "mainmenu" to the nav ul
 var urlString = document.location.href;
@@ -27,21 +43,6 @@ function toggleNavMenu() {
         x.className = "show";
     }
 } // end of function
-
-
-
-// --------------- GENERATE DATE SCRIPT --------------------
-var theDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
-var theMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-var d = new Date();
-var dayName = theDays[d.getDay()];
-var day = d.getDate();
-var monthName = theMonths[d.getMonth()];
-var year = d.getFullYear();
-
-document.getElementById("currentdate").innerHTML = dayName + ", " + day + " " + monthName + " " + year;
 
 // --------------- WINDCHILL SCRIPT --------------------
 var high = 90;
