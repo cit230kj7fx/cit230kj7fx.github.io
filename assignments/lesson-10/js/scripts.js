31616b9b73417d34706b3daa7cf2ce1f1515e5ef
Mike Odom <odommi@byui.edu>
2018-06-26 20:27:42 -0600

    Update

--- assignments/lesson-10/js/scripts.js
+++ assignments/lesson-10/js/scripts.js
@@ -41,3 +41,15 @@ windChill = Math.round(windChill) + "&deg;F";
 
 document.getElementById("windChill").innerHTML = windChill;
 
+// --------------- GENERATE DATE SCRIPT --------------------
+var theDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+var theMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+var d = new Date();
+var dayName = theDays[d.getDay()];
+var day = d.getDate();
+var monthName = theMonths[d.getMonth()];
+var year = d.getFullYear();
+
+document.getElementById("currentdate").innerHTML = dayName + ", " + day + " " + monthName + " " + year;
---
+++ assignments/lesson-10/js/weatherapi.js
@@ -0,0 +1,14 @@
+//Retrieve Weather Data for Franklin
+var weatherRequest = new XMLHttpRequest();
+var urlAPI="//api.openweathermap.org/data/2.5/forecast?id=4759986&APPID=ce06eddfee2de59e3e3a0fabeee489e1&units=imperial";
+weatherRequest.open('GET', urlAPI, true);
+
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    var weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+        
+    document.getElementById("current-temp").innerHTML = weatherData.list["0"].main.temp;
+    
+} //end of onload function
