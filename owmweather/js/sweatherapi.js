e2b6a9972a98f2cd76bc016dfea78946155db45a
Mike Odom <odommi@byui.edu>
2018-07-20 19:24:39 -0600

    owm update

---
+++ owmweather/js/sweatherapi.js
@@ -0,0 +1,16 @@
+//Retrieve Weather Data for Franklin
+var weatherRequest = new XMLHttpRequest();
+var urlAPI="//api.openweathermap.org/data/2.5/forecast?id=4409896&APPID=ce06eddfee2de59e3e3a0fabeee489e1&units=imperial";
+weatherRequest.open('GET', urlAPI, true);
+
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+    var weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+        
+    document.getElementById("current-temp").innerHTML = weatherData.list["0"].main.temp;
+    document.getElementById("hitemp").innerHTML = weatherData.list["0"].main.temp_max;
+    document.getElementById("lotemp").innerHTML = weatherData.list["0"].main.temp_min;
+    document.getElementById("wind_spd").innerHTML = weatherData.list["0"].wind.speed;
+} //end of onload function
